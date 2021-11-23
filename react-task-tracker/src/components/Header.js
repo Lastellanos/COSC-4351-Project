bdf77161af7d2847cb8214f5399bf6e5cf39e474
Brian Vasquez <alexandervasquez0082@gmail.com>
2021-11-23 17:43:57 -0600

    Starting File

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+const headingStyle = {
+    color:'red',backgroudnColor: 'black'
+}
+export default Header
