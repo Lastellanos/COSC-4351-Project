c088c1bb6ef75a4e3dcb650c9213cdc492eee848
Brian Vasquez <alexandervasquez0082@gmail.com>
2021-12-04 01:14:19 -0600

    new update, register

--- react-task-tracker/frontend/src/components/RegistrationForm.js
+++ react-task-tracker/frontend/src/components/RegistrationForm.js
@@ -2,8 +2,57 @@ import React from 'react'
 
 export const RegistrationForm = () => {
     return (
-        <div>
-            
+        <div className=" d-flex justify-content-center align-items-center container-lg ">
+
+            <form className="row g-3 mt-3">
+                <div class="row">
+                    <div class="col">
+                        <label htmlFor="inputFirst4" className="form-label">First Name</label>
+                        <input
+                            type="text" className="form-control" id="inputFirst4" />
+
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="col">
+                        <label htmlFor="inputLast4" className="form-label">Last Name</label>
+                        <input
+                            type="text" className="form-control" id="inputLast4" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div className="col">
+                        <label htmlFor="inputEmail4" className="form-label">Email</label>
+                        <input
+                            type="email" className="form-control" id="inputEmail4" />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div class="col">
+                        <label htmlFor="inputPhone4" className="form-label">Password</label>
+                        <input
+                            type="password" className="form-control" id="inputPhone4" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="col">
+                        <label htmlFor="inputPhone4" className="form-label">Confirm Password</label>
+                        <input
+                            type="password" className="form-control" id="inputPhone4" />
+                    </div>
+                </div>
+
+
+                <div className="col-12">
+                    <p />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+
+                    <p />
+                </div>
+
+            </form>
+
         </div>
     )
 }
--- react-task-tracker/frontend/src/pages/Register.js
+++ react-task-tracker/frontend/src/pages/Register.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from "../components/Hero";
+import { RegistrationForm } from '../components/RegistrationForm';
  const Register = () => {
     return (
+        <>
         <Hero hero="roomsHero">
 
         </Hero>
+        <RegistrationForm/>
+</>
     )
 }
 
