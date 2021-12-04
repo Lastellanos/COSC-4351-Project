c094e07301b52c13b84a6435b0785105d9a82a59
Lastellanos <70247809+Lastellanos@users.noreply.github.com>
2021-12-03 22:19:31 -0600

    final push

--- react-task-tracker/potentialBackend/index.js
+++ react-task-tracker/potentialBackend/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
 
-export const index = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "RestaurantApp",
+});
+
+app.post('/Reserve' , (req, res)=> {
+    
+const firstname = req.body.firstName;
+const lastname = req.body.lastName;
+const email = req.body.email;
+const phone = req.body.phone;
+const numGuests = req.body.numGuests;
+const time = req.body.time;
+
+    db.query("INSERT INTO table_reservation (name, phone_number, email, date, time, number_of_seats, availability VALUES (? , ?)", 
+        [name, phone_number, email, date, time, number_of_seats, availability], 
+        (err, result)=> {
+            console.log(err);
+        }
+        );
+});
+
+app.listen(3001, ()=> {
+    console.log("running server");
+})
+
+
